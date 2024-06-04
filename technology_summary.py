8daec2d0eecb6010534dc22a8c5a72291649425a
lifelonglearning123 <chao@macaws.ai>
2024-06-04 16:34:54 +0100

    technology_summary.py

--- technology_summary.py
+++ technology_summary.py
@@ -7,7 +7,7 @@ def summary(full_response):
     full_response = full_response + "Write a paragraph in 200 words summarising the technology.Do not use bullet points"
     #print(full_response)
     response = openai.chat.completions.create(
-        model="gpt-4", 
+        model="gpt-4o", 
         messages=[
             {   "role": "system",
             "content" : "You are a technology reviewer. Your audience is technology enthusiast. ",
